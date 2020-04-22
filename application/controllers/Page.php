04fc6f73bdc22b5e6e08811e77789d919eae4b91
salsabilarinaldi <42767645+salsabillarinaldi@users.noreply.github.com>
2020-04-22 13:12:34 +0700

    Update Page.php

--- application/controllers/Page.php
+++ application/controllers/Page.php
@@ -11,7 +11,19 @@ class Page extends MY_Controller {
 		// function render_backend tersebut dari file core/MY_Controller.php
 		$this->render_backend('home'); // load view home.php
 	}
-		
+
+	public function kontak(){
+		// function render_backend tersebut dari file core/MY_Controller.php
+		$this->render_backend('kontak'); // load view kontak.php
+	}
+	
+	public function indexobat()
+	{
+		$data_obat = $this->UserModel->Getobat_nib();
+		$data_jenis = $this->UserModel->Getjenis_nib();
+		$this->render_backend('obat',['data'=>$data_obat,'dataJ'=>$data_jenis]);
+	}
+
 
 }
 
