6a81d4f4cf6e1eef94bf265625f3c848f5ceaed6
salsabilarinaldi <42767645+salsabillarinaldi@users.noreply.github.com>
2020-04-24 19:57:13 +0700

    Update index.php

--- application/views/template/backend/index.php
+++ application/views/template/backend/index.php
@@ -48,6 +48,74 @@
         min-height: 2000px;
         padding-top: 70px;
     }
+    .footer {
+            padding: 50px 0 20px 0;
+            background-color: #35404f;
+            color: #878c94;
+            position: absolute;
+            left: 0;
+            width: 100%;
+        }
+        
+        .footer .title {
+            text-align: left;
+            color: #fff;
+            font-size: 25   px;
+        }
+        
+        .footer .social-icon {
+            padding: 0px;
+            margin: 0px;
+        }
+        
+        .footer .social-icon a {
+            display: inline-block;
+            color: #fff;
+            font-size: 25px;
+            padding: 5px;
+        }
+        
+        .footer .acount-icon a {
+            display: block;
+            color: #fff;
+            font-size: 18px;
+            padding: 5px;
+            text-decoration: none;
+        }
+        
+        .footer .acount-icon .fa {
+            margin-right: 25px;
+        }
+        
+        .footer .category a {
+            text-decoration: none;
+            color: #fff;
+            display: inline-block;
+            padding: 5px 20px;
+            margin: 1px;
+            border-radius: 4px;
+            margin-top: 6px;
+            background-color: #660000;
+        }
+        
+        .footer .payment {
+            margin: 0px;
+            padding: 0px;
+            list-style-type: none
+        }
+        
+        .footer .payment li {
+            list-style-type: none
+        }
+        
+        .footer .payment li a {
+            text-decoration: none;
+            display: inline-block;
+            color: #fff;
+            float: left;
+            font-size: 25px;
+            padding: 10px 10px;
+        }
     </style>
 </head>
 
@@ -72,7 +140,61 @@
         echo $contentnya;
         ?>
     </div>
+    <br />
+    <br />
+    <br />
+    <footer class="footer">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-3">
+                    <h4 class="title">Call A Doc</h4>
+                    <p>Call A Doc is a Web-based Application that provides services to buy medicines and ask the Doctor.</p>
+                    <span class="acount-icon"> 
+                        <a href="<?php echo base_url('page/aboutus'); ?>">About us</a>
+                        <a href="<?php echo base_url('page/kontak'); ?>">Contact us</a>
+                    </span>
 
+                    <ul class="social-icon">
+                        <a href="#" class="social"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+                        <a href="#" class="social"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+                        <a href="#" class="social"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+                        <a href="#" class="social"><i class="fa fa-youtube-play" aria-hidden="true"></i></a>
+                        <a href="#" class="social"><i class="fa fa-google" aria-hidden="true"></i></a>
+                        <a href="#" class="social"><i class="fa fa-dribbble" aria-hidden="true"></i></a>
+                    </ul>
+                </div>
+                <div class="col-sm-3">
+                    <h4 class="title">Lainnya</h4>
+                    <span class="acount-icon">          
+            <a href="#"><i class="fa fa-info-circle" aria-hidden="true"></i>Terms and Conditions</a>
+            <a href="#"><i class="fa fa-lock" aria-hidden="true"></i>Privacy</a>
+            <a href="#"><i class="fa fa-stethoscope" aria-hidden="true"></i>Join the Doctor's team</a>
+            <a href="#"><i class="fa fa-globe" aria-hidden="true"></i>Language</a>           
+          </span>
+                </div>
+                <div class="col-sm-3">
+                    <h4 class="title">Chategory</h4>
+                    <div class="category">
+                        <a href="<?php echo base_url('page/indexobat'); ?>">Medicine</a>
+                        <a href="<?php echo base_url('page/indexdokter'); ?>">Doctor</a>
+                    </div>
+                </div>
+                <div class="col-sm-3">
+                    <h4 class="title">Payment Methods</h4>
+                    <ul class="payment">
+                        <li><a href="#"><i class="fa fa-cc-amex" aria-hidden="true"></i></a></li>
+                        <li><a href="#"><i class="fa fa-credit-card" aria-hidden="true"></i></a></li>
+                        <li><a href="#"><i class="fa fa-paypal" aria-hidden="true"></i></a></li>
+                        <li><a href="#"><i class="fa fa-cc-visa" aria-hidden="true"></i></a></li>
+                    </ul>
+                </div>
+            </div>
+            <hr>
+
+            <div class="row text-center"> Copyright © 2020, CallADoc. All Rights Reserved.</div>
+        </div>
+
+    </footer>
     <!-- Load file Javascript Bootstrap & jQuery -->
     <!--<script src="<?php echo base_url('js/jquery.min.js'); ?>"></script>
     <script src="<?php echo base_url('js/bootstrap.min.js'); ?>"></script>-->
