6a2d8dcf75b52bc2219c16732585c011f832d9ec
salsabilarinaldi <42767645+salsabillarinaldi@users.noreply.github.com>
2020-04-25 02:20:10 +0700

    commit

--- application/views/reservasi.php
+++ application/views/reservasi.php
@@ -1,10 +1,12 @@
-  <div class="pull-right">
-  
-</div>
 
-<h2 style="margin-top: 0;margin-bottom: 0;">Data Appointment</h2>
+<?php
+if($this->session->flashdata('message')){ 
+  echo '<div class="alert alert-success">'.$this->session->flashdata('message').'</div>'; 
+}
+?>
+<h2 style="margin-top: 0;margin-bottom: 0;">Data Perjanjian</h2>
 <div class="clearfix"></div>
-<hr/>
+<hr>
 <table class="table table-bordered" id="table">
     <thead>
         <tr>
@@ -22,13 +24,12 @@
     <tbody>
         <?php $no=1; foreach ($data as $d ) {?>
             <tr>
-                <!--HINT UNTUK MENGHAPUS USER KALIAN DAPAT MENGGUNAKAN FORM, MENGGUNAKAN ANCHOR ATAU HREF PADA BUTTON-->
                 <form action="">
                     <td>
                         <?php echo $no++ ?>
                     </td>
                     <td>
-                        <?php echo $d->nid ?>
+                       DK <?php echo $d->nid ?>
                     </td>
                     <td>
                         <?php echo $d->nama ?>
@@ -48,7 +49,9 @@
                     <td>
                         <?php echo $d->jam ?>
                     </td>
-                    <td><a type="button" class="btn btn-danger" href="<?php echo base_url('page/reservasii/'.$d->nid.'/'.$d->noreservasi); ?>" onClick="return confirm('Apakah Anda Yakin?')">Mark As Done</a></td>
+                    <td>
+                         <button type="button" class="btn btn-warning" data-toggle="modal" data-target="#pesan<?php echo $d->nid ?>">Balas</button>
+                        <a type="button" class="btn btn-danger" href="<?php echo base_url('page/hapusreservasi/'.$d->nid.'/'.$d->noreservasi); ?>" onClick="return confirm('Apakah Anda Yakin?')">Mark As Done</a></td>
                 </form>
             </tr>
             <?php } ?>
@@ -57,6 +60,68 @@
 </div>
 </div>
 
+ <?php $no=1; foreach ($data as $d ) {?>
+        <div class="modal fade" id="pesan<?php echo $d->nid ?>" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <div class="box-header">
+                            <i class="fa fa-envelope"></i>
+                            <h3 class="box-title">Buat Reservasi</h3>
+                        </div>
+                        <div class="modal-body">
+                            <form method="post" action="<?php echo base_url('page/kirimpesan/'.$d->nid); ?>">
+                                <input type="text" class="form-control" id="formGroupExampleInput" placeholder="" name="idpengirim" value="<?php echo $d->idpengirim?>" readonly>
+                                <div class="form-group">
+                                    <label for="formGroupExampleInput">Email To:</label>
+                                    <input type="text" class="form-control" name="nama" placeholder="nama" value="<?php echo $d->pengirim?>" readonly>
+                                </div>
+                                <div class="form-group">
+                                    <label for="formGroupExampleInput">Subject</label>
+                                    <input type="text" class="form-control" id="formGroupExampleInput" placeholder="Subject" name="subjek" value="Appointment" readonly>
+                                </div>
+                                <div class="form-group">
+                                    <label for="formGroupExampleInput">Pengirim</label>
+                                    <input type="text" class="form-control" id="formGroupExampleInput" placeholder="Pengirim" name="pengirim" value="<?php echo $this->session->userdata('nama') ?>" readonly>
+                                </div>
+                                <div>
+                                    <label for="formGroupExampleInput">Pesan & Info Kontak</label>
+                                    <textarea class="textarea" placeholder="Pesan dan kontak yang bisa di hubungi" name="pesan" style="width: 100%; height: 125px; font-size: 14px; line-height: 18px; border: 1px solid #dddddd; padding: 10px;
+                  "></textarea>
+                                </div>
+                                <label for="sel1">Tanggal:</label>
+                                <input id="datepicker" width="270" name="tanggal" value="<?php echo $d->tanggal?>" />
+                                <div class="form-group">
+                                    <label for="sel1">Jam:</label>
+                                    <select class="form-control" id="jam" name="jam" value="<?php echo $d->jam?>">
+                                        <option>14.00</option>
+                                        <option>15.00</option>
+                                        <option>16.00</option>
+                                        <option>20.00</option>
+                                    </select>
+                                </div>
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal" style="margin-right: 3px;">Cancel</button>
+
+                        <button type="submit" class="pull-right btn btn-default" id="sendEmail">Send
+                            <i class="fa fa-arrow-circle-right"></i></button>
+
+                    </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+        <?php } ?>
+         <script type="text/javascript">
+                $(document).ready(function() {
+                    $('#table').DataTable();
+                });
+                $('#datepicker').datepicker({
+                    uiLibrary: 'bootstrap'
+                });
+            </script>
 
 
         <br>
